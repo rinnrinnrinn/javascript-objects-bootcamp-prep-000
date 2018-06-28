4701598a2a2305ac6b17bb7722b6d1f78b50df4a
Caitlin Lauer <caitlinrinn@gmail.com>
2018-06-28 15:20:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
+var playlist = {Beyonce:"single ladies", Spoon:"song", beatles:"yellow submarine"};
+
 Object.assign({}, playlist, {Beyonce: "lemonade"})
+
+function updatePlaylist(playlist, ArtistName, SongTitle) {
+  
+}
